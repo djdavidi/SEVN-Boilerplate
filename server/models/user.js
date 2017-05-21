91e1873175c1dd64e6a4dff947075c47b6bd5550
[David Scherban] <davidscherban@gmail.com>
2017-05-21 19:23:52 -0400

    partially thorugh user

---
+++ server/models/user.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        is: /^[a-z0-9\_\-]+$/i,
+        notEmpty: true,
+        len: [1,50]
+      }
+    },
+    email:{
+      type:  DataTypes.STRING,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: DataTypes.STRING
+  }, {
+    underscored: true,
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return User;
+};
