f3e97b3143a935b67d60190b806bd444e3db2acf
[David Scherban] <davidscherban@gmail.com>
2017-06-06 09:20:30 -0400

    setup still

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // Express, framework to handle routing, request, response cycle and more
 const express = require('express');
 // morgan logs out information about your server activity
@@ -8,9 +9,10 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const env = require('dotenv').load();
 
 const app = express();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // need to be able to parse cookies to have a session
@@ -18,14 +20,22 @@ app.use(cookieParser());
 
 // use passport to handle user sessions to keep track of them
 // while logged in
+// leave this fake secret in here for now until add in the env file before prod
 app.use(session({ secret: 'fourteenseventyfiveandttwentyairflightiniters' }));
 app.use(passport.initialize());
 app.use(passport.session());
+// app.use(express.static(path.join(__dirname, '..', '..', 'client')));
 
 // Require our routes into the application.
+
 require('./server/routes')(app, passport);
-app.get('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the beginning of nothingness.',
+
+app.get("*", (req, res) => res.status(200).send({
+  message: "Welcome to the SEVN Boilerplate",
 }));
+
+app.use(function(err, req, res, next) {
+	res.status(500).send({err: err});
+});
 
 module.exports = app;
